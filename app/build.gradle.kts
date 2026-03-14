b226aac57bafbc830af413fe5fc858339b49c0ba
Ashish Ekka <ashishekka@Ashishs-MacBook-Air.local>
2026-03-14 18:17:34 +0530

    Fix workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "me.ashishekka.mori"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version =
+            release(36) {
+                minorApiLevel = 1
+            }
     }
 
     defaultConfig {
@@ -26,7 +29,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
